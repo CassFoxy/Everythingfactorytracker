3b13810a82d4f2fbbb7ea917c1a75860579d10c0
CassFoxy <cassfoxy88@gmail.com>
2026-06-24 01:33:27 +0100

    Update ores.js

--- game/ores.js
+++ game/ores.js
@@ -1,1 +1,27 @@
+const ORES = {
 
+    amber: {
+        name: "Amber",
+        emoji: "🟠",
+        value: 10,
+        tier: 1,
+        xp: 5
+    },
+
+    amethyst: {
+        name: "Amethyst",
+        emoji: "💜",
+        value: 40,
+        tier: 1,
+        xp: 5
+    },
+
+    malachite: {
+        name: "Malachite",
+        emoji: "🟢",
+        value: 50,
+        tier: 1,
+        xp: 5
+    }
+
+};
